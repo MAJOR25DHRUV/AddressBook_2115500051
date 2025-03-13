697fc62d5aefc5f2934a6fba73504e67b718bf37
Dhruv kushwah <dhruvkushwah243@gmail.com>
2025-03-13 22:32:06 +0530

    [Dhruv] Add. Added UC1 of Section1

---
+++ RepositoryLayer/Context/AddressBookContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RepositoryLayer.Entity;   
+
+namespace RepositoryLayer.Context
+{
+    public class AddressBookContext : DbContext
+    {
+        public AddressBookContext(DbContextOptions<AddressBookContext> options) : base(options)
+        {
+        }
+
+        public DbSet<AddressBookEntity> AddressBooks { get; set; }
+    }
+}
