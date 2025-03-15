16beba1f663bba8aa24f5f3cea22c59a976bb6ad
Dhruv kushwah <dhruvkushwah243@gmail.com>
2025-03-16 00:24:13 +0530

    [Dhruv] Add. Added UC2 of Section 4

--- AddressBookApplication/Program.cs
+++ AddressBookApplication/Program.cs
@@ -30,7 +30,7 @@ builder.Services.AddSingleton<IMapper>(sp =>
     return config.CreateMapper();
 });
 
-// Add FluentValidation
+
 builder.Services.AddControllers()
     .AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<AddressBookValidator>());
 
@@ -62,8 +62,10 @@ builder.Services.AddScoped<IUserRL, UserRL>();
 builder.Services.AddScoped<IAddressBookBL, AddressBookBL>();
 builder.Services.AddScoped<IUserBL, UserBL>();
 builder.Services.Configure<SmtpSettings>(builder.Configuration.GetSection("SMTP"));
-builder.Services.AddScoped<EmailService>();
+builder.Services.AddSingleton<EmailService>();
 builder.Services.AddScoped<RedisCacheHelper>();
+builder.Services.AddSingleton<RabbitMQProducer>();
+builder.Services.AddHostedService<RabbitMQConsumer>();
 
 // Configure Database Context
 var connectionString = builder.Configuration.GetConnectionString("SqlConnection");
